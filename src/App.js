35e7c0fb901805b9b4b605fd22e7547f0016a5d5
dschmaryl <dschmaryl@gmail.com>
2018-02-16 16:41:22 -0500

    remove console logs

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ export class App extends React.Component {
   }
 
   controlsClick(control) {
-    console.log('controlsClick func called');
     switch (control) {
       case 'workMinus':
         this.setState({
