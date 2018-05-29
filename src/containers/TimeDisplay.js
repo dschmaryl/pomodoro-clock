b9f0af54f2dc7c7e9bb56b5753f639e961c02448
dschmaryl <dschmaryl@gmail.com>
2018-05-29 11:31:00 -0400

    set interval only if paused is false

--- src/containers/TimeDisplay.js
+++ src/containers/TimeDisplay.js
@@ -9,13 +9,29 @@ class TimeDisplay extends React.Component {
   constructor(props) {
     super(props);
     this.timerClick = this.timerClick.bind(this);
-    this.state = {
-      interval: setInterval(() => props.dispatch({ type: 'TIMER_TICK' }), 1000)
-    };
+    this.state = { interval: null, paused: true };
+  }
+
+  componentWillReceiveProps({ paused }) {
+    if (paused && !this.state.paused) {
+      this.setState({
+        interval: clearInterval(this.state.interval),
+        paused: true
+      });
+    } else if (!paused & this.state.paused) {
+      this.props.dispatch({ type: 'TIMER_TICK' });
+      this.setState({
+        interval: setInterval(
+          () => this.props.dispatch({ type: 'TIMER_TICK' }),
+          1000
+        ),
+        paused: false
+      });
+    }
   }
 
   timerClick() {
-    if (this.props.paused) {
+    if (this.state.paused) {
       this.props.dispatch({ type: 'UNPAUSE_TIMER' });
     } else {
       this.props.dispatch({ type: 'PAUSE_TIMER' });
