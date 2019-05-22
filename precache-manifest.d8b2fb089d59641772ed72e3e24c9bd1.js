ce5b6be7a8d4c815307a439dd5a49e548e0b2562
dschmaryl <dschmaryl@gmail.com>
2019-05-22 17:32:20 -0400

    Updates

---
+++ precache-manifest.d8b2fb089d59641772ed72e3e24c9bd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5feefbfab3a00801652e11049f78ce32",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "b536cbbc2299479da538",
+    "url": "/pomodoro-clock/static/css/main.8d595a38.chunk.css"
+  },
+  {
+    "revision": "46f3952f8aea69061003",
+    "url": "/pomodoro-clock/static/js/2.030df1f6.chunk.js"
+  },
+  {
+    "revision": "b536cbbc2299479da538",
+    "url": "/pomodoro-clock/static/js/main.69d975a0.chunk.js"
+  },
+  {
+    "revision": "aa817215ef5e490bad59",
+    "url": "/pomodoro-clock/static/js/runtime~main.647dc629.js"
+  }
+]);
